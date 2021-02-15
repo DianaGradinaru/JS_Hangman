7d8f58e2e86a8d2a28f97a0f3b931e185ee6efd2
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2021-02-15 12:30:01 +0200

    Initial setup

--- game.js
+++ game.js
@@ -1,7 +1,49 @@
-initGame();
 
 function initGame() {
+    alert("Let's play hangman")
+};  
+initGame();
 
-    // Your game can start here, but define separate functions, don't write everything in here :)
+var words = [
+    "codecool",
+    "kahoot",
+    "journey",
+    "attendance",
+    "workshop",
+    "teamwork",
+    "student",
+    "codecooler",
+    "assessment",
+    "project",
+];
 
+var word = words[Math.floor(Math.random() * words.length)];
+
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "_";
 }
+
+var remainingLetters = word.length;
+while (remainingLetters > 0) {
+    alert('Your word is ' + answerArray.join(" "));
+
+    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
+    if (guess === null) {
+        break;
+    } else if (guess.length !== 1) {
+        alert("Please enter a single letter.");
+    } else {
+        for (var j = 0; j < word.length; j++) {
+            if (word[j] === guess) {
+                answerArray[j] = guess;
+                remainingLetters--;
+            }
+        }
+    }
+}
+
+alert(answerArray.join(" "));
+alert("Good job! The answer was " + word);
+
+
