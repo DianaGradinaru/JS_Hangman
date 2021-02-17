dcd123bb556d7d2848ec31059c86ec34e86bed57
madalinagrama <m.madalinagrama@gmail.com>
2021-02-17 13:57:36 +0200

    little styling

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
 };
 window.onclick = function (event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
 };
@@ -28,21 +28,21 @@ let guessed;
 
 function initGame() {
     word = words[Math.floor(Math.random() * words.length)];
-    guessed = []
+    guessed = [];
 
     showWord();
     
     document
         .querySelectorAll("ul.alphabet li")
         .forEach((li) => li.addEventListener("click", selectLetter));
-};
+}
 
 initGame(); 
 
 
 function hasWon() {
-    let unGuessed = guessed.filter((l) => !word.includes(l))
-    var x = document.querySelector("#word").innerText.replace(/[ _]+/g, "");
+    let unGuessed = guessed.filter((l) => !word.includes(l));
+    let x = document.querySelector("#word").innerText.replace(/[ _]+/g, "");
         if (unGuessed.length < tries) {
         if (x === word) {
             document.querySelector("#theWord").innerText = word;
@@ -56,33 +56,30 @@ function hasWon() {
 function showGallows() {
     let unGuessed = guessed.filter((l) => !word.includes(l)).length + 1;
     let cls = "try" + unGuessed;
-    document.querySelector("#gallows").classList.add(cls)
-
+    document.querySelector("#gallows").classList.add(cls);
 }
 
-function showguessed() {
+function showGuessed() {
     document.querySelector("#guessed").innerText = guessed.filter(l => !word.includes(l)).join(" ");
-
 }
 
 function showWord() {
-    var shownWord = word
+    document.querySelector("#word").innerText = word
         .split("")
         .map((l) => (guessed.includes(l) ? l : "_"))
         .join(" ");
-    document.querySelector("#word").innerText = shownWord;
 }
 
 function selectLetter(event) {
     event.preventDefault();
 
-    var letter = event.target.innerText;
+    let letter = event.target.innerText;
     guessed.push(letter.toLowerCase());
 
     event.target.parentElement.removeChild(event.target);
 
     showWord();
-    showguessed();
+    showGuessed();
     showGallows();
     hasWon();
 }
